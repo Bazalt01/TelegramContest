ef667a7e4a942b766d252a3770d94fd5b15160d0
Gleb Tokmakov <tokmakov.gleb@gmail.com>
2019-03-13 15:03:07 +0300

    Initial Commit

---
+++ TelegramContest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelegramContest
+//
+//  Created by g.tokmakov on 13/03/2019.
+//  Copyright © 2019 g.tokmakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
